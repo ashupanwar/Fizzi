33c91a3ccefa4c94c5aa3d46ce52f60f8601f38e
Ashu Panwar <ashupanwar@Ashus-MacBook-Pro-2.local>
2026-04-10 22:55:49 +0530

    created gsap animations for hero

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,27 +1,101 @@
 import React from "react";
 import "../Styles/hero.scss";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { TextSplitter } from "./TextSplitter";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { Colors } from "../Contants/Color/Color";
+
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 const Hero = () => {
+  useGSAP(() => {
+    const introT1 = gsap.timeline();
+    introT1
+      .set(".hero-wrapper", { opacity: 1 })
+      .from(".hero-header-word", {
+        scale: 2,
+        opacity: 0,
+        ease: "power4.in",
+        delay: 0.2,
+        stagger: 1,
+      })
+      .from(
+        ".secondary-heading",
+        {
+          opacity: 0,
+          y: 30,
+        },
+        "+=0.8",
+      )
+      .from(".subtitle", {
+        opacity: 0,
+        y: 10,
+      })
+      .from(".cta", {
+        opacity: 0,
+        y: 10,
+        duration: 0.6,
+      });
+
+    const scrollT1 = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".hero-wrapper",
+        start: "top top",
+        end: "bottom bottom",
+        scrub: 1.5,
+        markers: true,
+      },
+    });
+
+    scrollT1
+      .fromTo(
+        "body",
+        {
+          backgroundColor: Colors.yellowBackground,
+        },
+        {
+          backgroundColor: Colors.greenBackground,
+          overwrite: "auto",
+        },
+        1,
+      )
+      .from(".primary-heading2 .split-char", {
+        scale: 1.3,
+        y: 40,
+        rotate: -25,
+        opacity: 0,
+        stagger: 0.1,
+        ease: "back.out(3)",
+        duration: 0.5,
+      })
+      .from(".subtitle2", {
+        y: 20,
+        opacity: 0,
+      });
+  });
+
   return (
     <div className="hero-wrapper">
-      <h1 className="primary-heading">
-        LIVE <br />
-        GUTSY
-      </h1>
-      <h3 className="secondary-heading">Soda Perfected</h3>
-      <p className="subtitle">
-        3-5g of sugar. 9g of fiber. 5 delicious flavors.
-      </p>
-      <button className="cta">SHOP NOW</button>
+      <div className="hero">
+        <h1 className="primary-heading">
+          <TextSplitter
+            text={`LIVE GUTSY`}
+            wordDisplayStyle="block"
+            className="hero-header-word"
+          />
+        </h1>
+        <h3 className="secondary-heading">Soda Perfected</h3>
+        <p className="subtitle">
+          3-5g of sugar. 9g of fiber. 5 delicious flavors.
+        </p>
+        <button className="cta">SHOP NOW</button>
+      </div>
 
       <div className="container1">
-        <h1 className="primary-heading2">
-          TRY ALL
-          <br />
-          FIVE
-          <br />
-          FLAVORS
-        </h1>
+        <h2 className="primary-heading2">
+          <TextSplitter text={`TRY ALL FIVE FLAVORS`} />
+        </h2>
         <p className="subtitle2">
           Our soda is made with real fruit juice and a touch of cane
           <br /> sugar. We never use artificial sweeteners or high fructose
--- src/Contants/Color/Color.js
+++ src/Contants/Color/Color.js
@@ -1,4 +1,6 @@
 export const Colors = {
   primaryColor: "#075985",
   primaryColorLight: "#FDE047",
+  yellowBackground: "#FDE047",
+  greenBackground: "#D9F99D",
 };
